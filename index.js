cc748e1349892169a5817bcae4219a1fbe53e1c4
Amit Datta <ak.datta2648@gamil.com>
2022-10-25 01:41:50 +0600

    Tech Academy Server Complete

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.get('/', (req, res) => {
     res.send('Tech Academy Server.');
 });
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:courseId', (req, res) => {
+    const courseId = req.params.courseId;
+    const course = courses.find( c => c.id === courseId);
+    res.send(course);
+});
+
 
 
 
